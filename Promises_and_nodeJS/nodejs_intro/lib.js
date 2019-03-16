ef26998e9c0d498dbede41d74b07f9fb93d3731e
Shreya Kapoor <shreyakapoor98@gmail.com>
2019-03-16 13:20:01 +0530

    node intro

---
+++ Promises_and_nodeJS/nodejs_intro/lib.js
@@ -0,0 +1,7 @@
+function awesomeFunction() {
+    console.log("this isss awesome")
+}
+
+module.exports = {  //creating an object and export this
+    awesomeFunction
+}
---
+++ Promises_and_nodeJS/nodejs_intro/script.js
@@ -0,0 +1,4 @@
+const lib = require('./lib.js') //require use karke import kiya hai
+
+console.log(lib)
+lib.awesomeFunction()
