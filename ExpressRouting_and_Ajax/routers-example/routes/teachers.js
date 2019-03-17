77790af28bad95d9d0ad7eb617e6b530598b9288
Shreya Kapoor <shreyakapoor98@gmail.com>
2019-03-17 23:32:56 +0530

    /:id changes

--- ExpressRouting_and_Ajax/routers-example/routes/teachers.js
+++ ExpressRouting_and_Ajax/routers-example/routes/teachers.js
@@ -13,7 +13,12 @@ route.get('/add', (req, res) => {
     })
     res.send(teachers)
 })
-route.get('/:id', (req, res) => res.send(teachers[req.params.id]))
+route.get('/:id', (req, res) => {
+    if (isNaN(parseInt(req.params.id))) {
+        next();
+    }
+    res.send(teachers[req.params.id])
+})
 
 
 module.exports = route
