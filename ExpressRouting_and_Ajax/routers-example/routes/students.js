4ba67e356d044fc439e54abc6301fba99a1a25f5
Shreya Kapoor <shreyakapoor98@gmail.com>
2019-03-18 12:24:50 +0530

    post req

--- ExpressRouting_and_Ajax/routers-example/routes/students.js
+++ ExpressRouting_and_Ajax/routers-example/routes/students.js
@@ -9,6 +9,13 @@ let students = [
 ]
 
 route.get('/', (req, res) => res.send(students))
+route.post('/', (req, res) => {
+    students.push({
+        name: req.body.name, //name is expected to come in body
+        college: req.body.college,
+        year: req.body.year
+    })
+})
 route.get('/:id', (req, res) => res.send(students[req.params.id]))
 
 module.exports = route
--- ExpressRouting_and_Ajax/routers-example/server.js
+++ ExpressRouting_and_Ajax/routers-example/server.js
@@ -1,7 +1,14 @@
-const srv = require('express')() //we can also call like this
+const express = require('express')
+const srv = express()
+//const srv = require('express')() //we can also call like this
 
 const teacherRoute = require('./routes/teachers')
 const studentRoute = require('./routes/students')
+
+//in our server we will use a middleware present inside express only
+srv.use(express.json()) //special middleware that can decode the body of a req and turn it into a js object if the body is encoded in json
+srv.use(express.urlencoded({extended: true})) //this middleware can decode the body if the body is in urlencoded format
+
 
 srv.use('/students', studentRoute)
 srv.use('/teachers', teacherRoute)
