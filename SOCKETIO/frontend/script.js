38cf28e5175047846731df0767c9b73fb1eacda0
Shreya Kapoor <shreyakapoor98@gmail.com>
2019-04-13 22:47:03 +0530

    advance socket start

--- SOCKETIO/frontend/script.js
+++ SOCKETIO/frontend/script.js
@@ -8,9 +8,24 @@ $(function () {
     let msglist = $('#msglist')
     let sendbtn = $('#sendmsg')
     let msgbox = $('#msgbox')
+    let loginbox = $('#loginbox')
+    let loginbtn = $('#loginbtn')
+    let loginDiv = $('#login-div')
+    let chatDiv = $('#chat-div')
+
+    let user = ''
 
     sendbtn.click(function () {
-        socket.emit('send_msg', {message: msgbox.val()})
+        socket.emit('send_msg', {
+            user: user,
+            message: msgbox.val()
+        })
+    })
+
+    loginbtn.click(function () {
+        user = loginbox.val()
+        chatDiv.show()
+        loginDiv.hide()
     })
 
     socket.on('recv_msg', function (data) {
